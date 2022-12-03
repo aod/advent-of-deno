a0600165d1a2772276183b97c1321b02df7e52f1
Oktay <oktay@yatko.dev>
2022-12-03 14:37:05 +0100

    Solve 2022/2

---
+++ src/2022/day02.ts
@@ -0,0 +1,46 @@
+import { sum } from "../math.ts"
+
+const LOSE = 0
+const DRAW = 3
+const WIN = 6
+const R = 1
+const P = 2
+const S = 3
+
+const points: Record<string, number> = {
+  "A X": R + DRAW,
+  "A Y": P + WIN,
+  "A Z": S + LOSE,
+
+  "B X": R + LOSE,
+  "B Y": P + DRAW,
+  "B Z": S + WIN,
+
+  "C X": R + WIN,
+  "C Y": P + LOSE,
+  "C Z": S + DRAW,
+}
+
+function part1(input: string) {
+  return sum(...input.split("\n").map(line => points[line]))
+}
+
+const choose: Record<string, string> = {
+  "A X": "A Z",
+  "A Y": "A X",
+  "A Z": "A Y",
+
+  "B X": "B X",
+  "B Y": "B Y",
+  "B Z": "B Z",
+
+  "C X": "C Y",
+  "C Y": "C Z",
+  "C Z": "C X"
+}
+
+function part2(input: string) {
+  return sum(...input.split("\n").map(line => points[choose[line]]))
+}
+
+export default { part1, part2 };
---
+++ src/2022/day02_test.ts
@@ -0,0 +1,19 @@
+import { assertEquals } from "../../deps.ts";
+import { input } from "../aoc.ts";
+import day02 from "./day02.ts";
+
+Deno.test("22-02a-ex1", () => {
+  assertEquals(day02.part1(input(2022, 2, "-ex1")), 15);
+});
+
+Deno.test("22-02a", () => {
+  assertEquals(day02.part1(input(2022, 2)), 12772);
+});
+
+Deno.test("22-02b-ex1", () => {
+  assertEquals(day02.part2(input(2022, 2, "-ex1")), 12);
+});
+
+Deno.test("22-02b", () => {
+  assertEquals(day02.part2(input(2022, 2)), 11618);
+});
